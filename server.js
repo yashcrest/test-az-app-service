539cce25bad7ed98f51125dc1ebe7f6ba417ae84
yash shrestha <yashcrest@gmail.com>
2024-07-07 14:27:56 +1000

    fixing port issue

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from "express";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json("this api is running");
 });
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
