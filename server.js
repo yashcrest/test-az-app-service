20cd486f1f1e995b3255b3af5ae05f0038f3bba3
yash shrestha <yashcrest@gmail.com>
2024-07-07 15:06:11 +1000

    adding index.html for testing

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.json("this api is running");
+});
+
+app.get("/test", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(port, () => {
