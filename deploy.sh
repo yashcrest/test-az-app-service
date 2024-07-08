50e436b8309adc4f6c8c408fdf90d320bd9c30a6
yash shrestha <yashcrest@gmail.com>
2024-07-08 13:05:31 +1000

    fix: deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,4 @@
 cd backend
 
 # run build command
-oryx build /home/site/repository -o /home/site/wwwroot --platform nodejs --platform-version 20 -p virtualenv_name= --log-file /tmp/build-debug.log  -i /tmp/8dc9ef722cc2f90 -p compress_node_modules=tar-gz | tee /tmp/oryx-build.log
-2024-07-08T02:39:08.546Z - Operation performed by Microsoft Oryx, https://github.com/Microsoft/Oryx
+oryx build /home/site/repository/backend -o /home/site/wwwroot --platform nodejs --platform-version 20 -p virtualenv_name= --log-file /tmp/build-debug.log  -i /tmp/8dc9ef722cc2f90 -p compress_node_modules=tar-gz | tee /tmp/oryx-build.log
